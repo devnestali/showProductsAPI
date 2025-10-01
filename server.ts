6ac23197592413a1b067c8e1a11f2d5fc46597fb
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-10-01 10:57:08 +0200

    feat: Configuring prisma ORM to use on the API

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
 const express = require("express")
 const app = express()
 
+const PORT = 3333
+
 app.use(express.json())
 
-const PORT = 3333
+
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT:${PORT}`)
