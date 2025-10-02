fe25b5bb860d4b9460b0791b8cdaa99c1d1539fd
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-10-02 11:28:40 +0200

    feat: Creating create method on OrderController

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -15,35 +15,32 @@ const ordersSchema = z.object({
 
 
 class OrderController {
-  async create(request: Request, response: Response) {
-    try {
-      const { title, description } = request.body
+  async create(request: Request, response: Response): Promise<void> {
+    const { title, description } = request.body
+    let userId = request.cookies.userId
+    
+    if(!userId) {
+      throw new Error('Usuário não autenticado.')
+    }
 
-      if(!title || !description) {
-        return new Error('Título e descrição devem ser preenchidos.')
+    userId = Number(userId)
+    
+    if(!title) {
+      throw new Error('Título deve ser preenchido.')
+    }
+    
+    const formattedTitle = title.trim()
+    const formattedDescription = description.trim()
+
+    await prisma.order.create({
+      data: {
+        title: formattedTitle,
+        description: formattedDescription,
+        userId: userId
       }
-      // preciso fazer o controller do usuario para continuar
+    })
 
-      const formattedTitle = title.trim()
-      const formattedDescription = description.trim()
-
-
-
-    } catch (error) {
-      return response.status(500).json({ message: 'Erro ao criar pedido.' })
-    }
-  }
-  
-  async show(request: Request, response: Response) {
-    try {
-      const orders = await prisma.order.findMany()
-      
-      const validatedOrders = ordersSchema.parse(orders)
-      
-      return response.status(200).json(validatedOrders)
-    } catch (error) { 
-      return response.status(500).json({ message: 'Erro ao buscar pedidos.' })
-    }
+    response.status(201).json({ message: 'Pedido criado com sucesso.' })
   }
 }
 
