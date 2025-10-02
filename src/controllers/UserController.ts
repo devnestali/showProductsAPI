8f8d31184cb1ce466abd1046a2d9a19fd305f764
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-10-02 13:17:54 +0200

    refactor: Refactoring create method on UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -4,36 +4,36 @@ import bcrypt from "bcrypt"
 import { prisma } from "../prisma.ts";
 
 class UserController {
-  async create(request: Request, response: Response) {
+  async create(request: Request, response: Response): Promise<void> {
     const { username, email, password, isAdmin } = request.body
 
     if(!isAdmin) {
-      return new Error("Somente administradores podem acessar essa página.")
+      throw new Error("Somente administradores podem acessar essa página.")
     }
 
     const emptyFields = !username || !email || !password
 
     if(emptyFields) {
-      return new Error("Preencha todos os campos antes de entrar")
+      throw new Error("Preencha todos os campos antes de entrar")
     }
 
     const emailAlreadyExists = await prisma.user.findUnique({ where: { email } })
 
     if (emailAlreadyExists) {
-      return new Error("O e-mail já existe.")
+      throw new Error("O e-mail já existe.")
     }
 
     const usernameAlreadyExists = await prisma.user.findUnique({ where: { username } })
 
     if (usernameAlreadyExists) {
-      return new Error("Já existe um usuário com esse nome.")
+      throw new Error("Já existe um usuário com esse nome.")
     }
 
     const formattedUsername = username.trim()
     const formattedEmail = email.trim()
     const hashedPassword = bcrypt.hashSync(password, 8)
 
-    await prisma.user.create({
+    const user = await prisma.user.create({
       data: {
         username: formattedUsername,
         email: formattedEmail,
@@ -42,8 +42,15 @@ class UserController {
       }
     })
 
-    return response.status(200).json({
-      message: 'Usuário registrado com sucesso.'
+    response.cookie("userId", user.id, {
+      httpOnly: true,
+      secure: false,
+      maxAge: 24 * 60  * 60 * 1000, // 1 day in ms
+      sameSite: "strict"
+    })
+
+    response.status(201).json({
+      message: 'Usuário registrado com sucesso.',
     })
 
   }
