bf541b3fad140af2d330c79307d74086201efa99
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-10-05 01:16:14 +0200

    refactor: Adding middleware in all routes on the application to verify user authentication

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,12 @@ import userRoutes from "./User.routes.ts";
 import orderRoutes from "./Order.routes.ts";
 import graphRoutes from "./Graph.routes.ts";
 
+import authUser from "../middlewares/authUser.ts";
+
 const routes = Router()
 
-routes.use('/user', userRoutes)
-routes.use('/order', orderRoutes)
-routes.use('/graph', graphRoutes)
+routes.use('/user', authUser, userRoutes)
+routes.use('/order', authUser, orderRoutes)
+routes.use('/graph', authUser, graphRoutes)
 
 export default routes
