aedaa26c87d54ba5a67c994746b6509fa1698f85
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-10-06 09:54:43 +0200

    feat: Creating session routes and add it on index of routes folder

---
+++ src/routes/Session.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import SessionController from "../controllers/SessionController.ts";
+
+const sessionRoutes = Router()
+const sessionController = new SessionController()
+
+sessionRoutes.post('/', sessionController.create.bind(sessionController))
+
+export default sessionRoutes
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,11 +5,13 @@ import orderRoutes from "./Order.routes.ts";
 import graphRoutes from "./Graph.routes.ts";
 
 import authUser from "../middlewares/authUser.ts";
+import sessionRoutes from "./Session.routes.ts";
 
 const routes = Router()
 
 routes.use('/user', authUser, userRoutes)
 routes.use('/order', authUser, orderRoutes)
 routes.use('/graph', authUser, graphRoutes)
+routes.use('/session', sessionRoutes)
 
 export default routes
